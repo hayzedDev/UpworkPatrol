3d610548f59986ec8baca4ff5de72ff268c4d802
Azeez <azeezade04@gmail.com>
2023-02-19 23:53:41 +0100

    Chore[main changes]

--- preload.js
+++ preload.js
@@ -6,4 +6,30 @@ contextBridge.exposeInMainWorld('electron', {
   startScraping() {},
   stopScraping() {},
   path: path.join(__dirname, 'index.css'),
+  setQueryString(query) {
+    const settings = JSON.parse(fs.readFileSync('./settings.json', 'utf-8'));
+
+    // check if there are more than one query string
+    if (settings.user.queryStrings.length >= 2) return false;
+    settings.user.queryStrings = [...settings.user.queryStrings, query];
+
+    fs.writeFileSync('./settings.json', JSON.stringify(settings));
+
+    return true;
+  },
+  deleteQueryString(query) {
+    const settings = JSON.parse(fs.readFileSync('./settings.json', 'utf-8'));
+
+    // remove from settings
+    settings.user.queryStrings = settings.user.queryStrings.filter(
+      (olderQueries) => olderQueries.toLowerCase() === query.toLowerCase()
+    );
+    // save the new changes
+    fs.writeFileSync('./settings.json', JSON.stringify(settings));
+    return true;
+  },
+
+  getQeuryStrings() {
+    return JSON.parse(fs.readFileSync('./settings.json', 'utf-8'));
+  },
 });
